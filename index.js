61a2126c0e282384ff7d0a20eecc189013894cd4
Adhouse28 <87209460+Adhouse28@users.noreply.github.com>
2025-06-30 14:49:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post("/api/shorturl", function (req, res) {
+  res.json({ original_url: req.body.url, short_url: 1 });
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
